886a7a657a61a5477f4eeddc98a32c4770490d3f
HarryBeesley-Gilman <harry.beesley.gilman@gmail.com>
2023-10-25 14:05:57 +0100

    changed port number

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def ask():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 7352)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
 
